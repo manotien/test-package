97d3e22b6279ec81fe69451f0f03ea011b2337d4
manotien <manotien@gmail.com>
2022-12-29 10:02:48 +0700

    chore: gitignore

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.printMsg = void 0;
-var printMsg = function () {
-    console.log("This is a message from the demo package");
-};
-exports.printMsg = printMsg;
